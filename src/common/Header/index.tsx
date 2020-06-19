a9c335e32dd9937293fff511cbd5021f72e6199a
Salah Karim <salahkarimka@gmail.com>
2020-06-19 12:58:51 +0200

    Add back function to header

--- src/common/Header/index.tsx
+++ src/common/Header/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styles from './styles.module.css'
 
 import IconMenu from '../../icons/burger_menu.svg'
 import IconLeftArrow from '../../icons/left_arrow.svg'
 
+import { AppContext } from '../../wrappers/context'
+
 function Header(props: { title: string; backButton?: boolean }) {
     const { title, backButton = false } = props
+    const { backScreen } = useContext(AppContext)
     return (
         <nav className={styles.header}>
-            {backButton ? <img className={`${styles['header__child']} ${styles['header__back-icon']}`} alt="Back" src={IconLeftArrow} /> : null}
+            {backButton ? <img onClick={() => backScreen()} className={`${styles['header__child']} ${styles['header__back-icon']}`} alt="Back" src={IconLeftArrow} /> : null}
             <h1 className={`${styles['header__child']}`}>{title}</h1>
             <img alt="menu" className={`${styles['header__child']} ${styles['header__burger-icon']}`} src={IconMenu} />
         </nav>
