4a69c34be542c95f7f65d0a3dfde5227a98ba79f
Salah Karim <salahkarimka@gmail.com>
2020-06-25 19:23:30 +0200

    clean CreditCardView

--- src/steps/CreditCardView/BodyCreditCard.tsx
+++ src/steps/CreditCardView/BodyCreditCard.tsx
@@ -1,29 +1,15 @@
 import React from 'react'
-import styles from './styles.module.css'
 import stylesCommon from '../../styles.module.css'
 
-import InputButton from '../../common/Input/InputButton'
 import InputText from '../../common/Input/InputText'
 
-import { ListItemType } from '../../common/types'
-
 type BodyBuyCryptoType = {
-    onBuyCrypto: () => void,
-    openPickCrypto: () => void,
-    openPickCurrency: () => void,
-    openPickPayment: () => void,
-    selectedCrypto: ListItemType,
-    selectedCurrency: ListItemType,
-    selectedPaymentMethod: ListItemType,
-    expectedAmount: number,
-    amountValue: number
+    onButtonAction: () => void
     handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void,
 }
 
 const BodyBuyCrypto: React.FC<BodyBuyCryptoType> = (props) => {
-    const { openPickCrypto, onBuyCrypto, openPickCurrency, openPickPayment } = props
-    const { selectedCrypto, selectedCurrency, selectedPaymentMethod, expectedAmount, amountValue } = props
-    const { handleInputChange } = props
+    const { handleInputChange, onButtonAction } = props
     return (
         <main className={stylesCommon.body}>
             <InputText name='card-number' onChange={handleInputChange} className={stylesCommon['body__child']} label="Card number" placeholder="" />
@@ -33,25 +19,14 @@ const BodyBuyCrypto: React.FC<BodyBuyCryptoType> = (props) => {
                 <InputText name='card-ccv' onChange={handleInputChange} className={stylesCommon['row-fields__child']} label="CCV" placeholder="123" />
             </div>
             <div className={`${stylesCommon['body__child']} ${stylesCommon.grow}`}>
-                <button className={`${stylesCommon['button-action']}`}>Continue</button>
+                <button onClick={onButtonAction} className={`${stylesCommon['button-action']}`}>Continue</button>
             </div>
         </main>
     )
 }
 
 BodyBuyCrypto.defaultProps = {
-    selectedCrypto: {
-        name: 'Loading...',
-        icon: ''
-    },
-    selectedCurrency: {
-        name: 'Loading...',
-        icon: ''
-    },
-    selectedPaymentMethod: {
-        name: 'Loading...',
-        icon: ''
-    },
+ 
 }
 
 export default BodyBuyCrypto
