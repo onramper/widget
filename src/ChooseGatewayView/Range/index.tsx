086ee5b52f2386c404a22dba3b218baebdfd7e67
Salah Karim <salahkarimka@gmail.com>
2020-07-07 17:45:24 +0200

    Fix range colors

--- src/ChooseGatewayView/Range/index.tsx
+++ src/ChooseGatewayView/Range/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import styles from './styles.module.css'
 
+const LEVEL_COLORS_CLASS = {
+    LOW: styles['color--low'],
+    MEDIUM: styles['color--medium'],
+    HIGH: styles['color--high']
+}
+
 type RangeType = {
     min: number,
     max: number,
@@ -10,20 +16,21 @@ type RangeType = {
 const Range: React.FC<RangeType> = (props) => {
     const { min, max, actual } = props
     const actualInRange = actual < min ? min : actual > max ? max : actual
-    const actialPosition = `${(actualInRange - min) / (max - min) * 100}%`
+    const actialPosition = (actualInRange - min) / (max - min) * 100
+    const actualLevel = actialPosition > 80 ? LEVEL_COLORS_CLASS.LOW : actialPosition > 40 ? LEVEL_COLORS_CLASS.MEDIUM : LEVEL_COLORS_CLASS.LOW
     return (
         <div>
             <div className={styles.range}>
-                <div className={styles.range__section} style={{ backgroundColor: 'green' }} ></div>
-                <div className={styles.range__section} style={{ backgroundColor: 'green' }} ></div>
-                <div className={styles.range__section} style={{ backgroundColor: 'gold' }} ></div>
-                <div className={styles.range__section} style={{ backgroundColor: 'gold' }} ></div>
-                <div className={styles.range__section} style={{ backgroundColor: 'orange' }} ></div>
+                <div className={`${LEVEL_COLORS_CLASS.LOW} ${styles.range__section}`} ></div>
+                <div className={`${LEVEL_COLORS_CLASS.LOW} ${styles.range__section}`} ></div>
+                <div className={`${LEVEL_COLORS_CLASS.MEDIUM} ${styles.range__section}`} ></div>
+                <div className={`${LEVEL_COLORS_CLASS.MEDIUM} ${styles.range__section}`} ></div>
+                <div className={`${LEVEL_COLORS_CLASS.HIGH} ${styles.range__section}`} ></div>
             </div>
             <div className={styles['range-label-container']} >
                 <span className={styles['range-label__min']} >{min}%</span>
-                <div style={{ left: actialPosition }} className={styles['range-label__actual']} >
-                    <span className={styles.bubble}>{actual}%</span>
+                <div style={{ left: `${actialPosition}%` }} className={styles['range-label__actual']} >
+                    <span className={`${styles.bubble} ${actualLevel}`}>{actual}%</span>
                 </div>
                 <span className={styles['range-label__max']} >{max}%</span>
             </div>
