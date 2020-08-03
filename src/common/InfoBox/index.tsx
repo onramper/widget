75f6d499c8ed3ad2d40363cfafa4fe027722d0d6
Salah Karim <salahkarimka@gmail.com>
2020-08-04 00:24:33 +0200

    Added error type on infobox

--- src/common/InfoBox/index.tsx
+++ src/common/InfoBox/index.tsx
@@ -1,18 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './styles.module.css'
+import { CSSTransition } from 'react-transition-group';
 
 type InfoBoxType = {
-    text: string
+    text?: string
+    type?: 'info' | 'error'
+    onClose?: () => void
+    canBeClosed?: boolean
 }
 
 const InfoBox: React.FC<InfoBoxType> = (props) => {
-    const { text } = props
+    const { text, type = 'info', onClose = () => null, canBeClosed = false } = props
+
+    const [isOpen, setIsOpen] = useState(props.text ? true : false)
+
+    useEffect(() => {
+        setIsOpen(props.text ? true : false)
+    }, [props.text])
+
+    let classBoxType = ''
+    switch (type) {
+        case 'error':
+            classBoxType = 'infobox--error'
+            break;
+        case 'info':
+        default:
+            classBoxType = 'infobox--info'
+            break;
+    }
+
+    const handleClose = () => {
+        setIsOpen(false)
+        onClose()
+    }
+
     return (
-        <p className={styles.infobox}>{text}</p>
+        <CSSTransition in={isOpen}
+            timeout={{
+                enter: 100,
+                exit: 0
+            }}
+            classNames={{
+                enter: styles['collapse-enter'],
+                enterActive: styles['collapse-enter-active'],
+
+            }}
+            unmountOnExit={true} >
+            <div className={`${styles.infobox} ${styles[classBoxType]}`}>
+                <span className={styles['text']}>{text}</span>
+                {canBeClosed && <span className={styles['close-button']} onClick={handleClose} >✖</span>}
+            </div>
+        </CSSTransition >
     )
 }
 
-InfoBox.defaultProps = {
-}
 
 export default InfoBox
