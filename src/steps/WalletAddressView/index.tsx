3f4b7ef5a05df2e438d984a6b80d39bc13a361a5
Salah Karim <salahkarimka@gmail.com>
2020-08-07 01:49:13 +0200

    Demoize

--- src/steps/WalletAddressView/index.tsx
+++ src/steps/WalletAddressView/index.tsx
@@ -2,24 +2,24 @@ import React, { useContext, useState, useEffect } from 'react';
 import Header from '../../common/Header'
 import Footer from '../../common/Footer'
 import BodyWalletAddress from './BodyWalletAddress'
-import Step from '../Step'
+/* import Step from '../Step' */
 import styles from '../../styles.module.css'
 
 import { NavContext } from '../../wrappers/context'
 import { APIContext } from '../../context'
 
-/* import PersonalDetailsView from './../PersonalDetailsView' */
+import PersonalDetailsView from './../PersonalDetailsView'
 
 const WalletAddressView: React.FC = () => {
   const { nextScreen } = useContext(NavContext);
-  const { inputInterface, data, collected } = useContext(APIContext);
+  const { inputInterface, /* data,  */collected } = useContext(APIContext);
   const [isFilled, setIsFilled] = useState(false)
 
-  const handleButtonAction = () => {
+/*   const handleButtonAction = () => {
     if (!collected.walletAddress || !data.nextStep) return
     if (collected.selectedGateway?.nextStep)
       nextScreen(<Step {...collected.selectedGateway?.nextStep} />)
-  }
+  } */
 
   useEffect(() => {
     const isFilled = collected.walletAddress ? true : false
@@ -30,7 +30,7 @@ const WalletAddressView: React.FC = () => {
     <div className={styles.view}>
       <Header title="Wallet address" backButton />
       <BodyWalletAddress
-        onActionButton={handleButtonAction}//{() => nextScreen(<PersonalDetailsView />)}
+        onActionButton={() => nextScreen(<PersonalDetailsView />)}
         handleInputChange={inputInterface.handleInputChange}
         isFilled={isFilled}
       />
