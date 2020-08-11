31845d7466a032c995fe0d29d9f1ef4d6ad64e68
Salah Karim <salahkarimka@gmail.com>
2020-08-11 13:37:51 +0200

    Fix Input hitbox

--- src/common/Input/InputButton.tsx
+++ src/common/Input/InputButton.tsx
@@ -15,9 +15,9 @@ type InputButtonType = {
 const InputButton: React.FC<InputButtonType> = (props) => {
     const { selectedOption, label, icon, className, iconPosition } = props
     return (
-        <div onClick={props.onClick} className={`${styles['input']} ${className}`}>
+        <div className={`${styles['input']} ${className}`}>
             {label ? <label>{label}</label> : null}
-            <div className={`${styles['input__type']} ${styles['input__type--selector']}`}>
+            <div onClick={props.onClick} className={`${styles['input__type']} ${styles['input__type--selector']}`}>
                 {icon ? <img alt="Icon" src={icon} className={`${styles['input__type__child']} ${styles.input__icon} ${iconPosition === 'end' ? styles['input__type__child--old-first'] : ''}`} /> : null}
                 <span style={{ 'order': iconPosition === 'end' ? -1 : 'unset' }} className={`${styles['input__type__child']} ${iconPosition === 'end' ? styles['input__type__child--new-first'] : ''}`}>{selectedOption}</span>
                 <img alt="Chevron right" src={IconChevronRight} className={`${styles['input__type__child']} ${styles.input__icon} ${styles['input__icon--chevron']}`} />
