32755255626e1ec8321084f367e4410ea382a267
Salah Karim <salahkarimka@gmail.com>
2020-08-21 00:13:32 +0200

    Disable pick from list when availableCryptos length === 1

--- src/common/Input/InputButton.tsx
+++ src/common/Input/InputButton.tsx
@@ -17,10 +17,10 @@ const InputButton: React.FC<InputButtonType> = (props) => {
     return (
         <div className={`${styles['input']} ${className}`}>
             {label ? <label>{label}</label> : null}
-            <div onClick={props.onClick} className={`${styles['input__type']} ${styles['input__type--selector']}`}>
+            <div onClick={props.onClick} className={`${styles['input__type']} ${styles['input__type--selector']} ${props.onClick ? '' : styles['input__type--selector--disabled']}`}>
                 {icon ? <img alt="Icon" src={icon} className={`${styles['input__type__child']} ${styles.input__icon} ${iconPosition === 'end' ? styles['input__type__child--old-first'] : ''}`} /> : null}
                 <span style={{ 'order': iconPosition === 'end' ? -1 : 'unset' }} className={`${styles['input__type__child']} ${iconPosition === 'end' ? styles['input__type__child--new-first'] : ''}`}>{selectedOption}</span>
-                <img alt="Chevron right" src={IconChevronRight} className={`${styles['input__type__child']} ${styles.input__icon} ${styles['input__icon--chevron']}`} />
+                {props.onClick ? <img alt="Chevron right" src={IconChevronRight} className={`${styles['input__type__child']} ${styles.input__icon} ${styles['input__icon--chevron']}`} /> : null}
             </div >
         </div >
     )
