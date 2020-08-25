b15741adb265dcd592e3c11b518d32a5be1d8989
Salah Karim <salahkarimka@gmail.com>
2020-08-25 02:01:20 +0200

    Add header + footer to StepView

--- src/steps/Step/index.tsx
+++ src/steps/Step/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import StepViewContent from './StepViewContent'
 import styles from '../../styles.module.css'
-
+import Header from '../../common/Header'
+import Footer from '../../common/Footer'
 import { NextStep } from '../../context'
 
 const StepView: React.FC<NextStep> = (props) => {
 
   return (
     <div className={styles.view}>
+      <Header title="Purchase step" backButton />
       <StepViewContent {...props} />
+      <Footer />
     </div>
   );
 };
