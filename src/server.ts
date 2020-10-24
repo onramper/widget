2c9faa45bdcc50ed5646857d73feb42ff5950c0b
corollari <github@albert.sh>
2020-10-24 07:18:44 +0200

    Remove unused file

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-// dev is the default value (in case no stage is set)
-const domain = process.env.REACT_APP_STAGE === "prod"? "onramper.com": "onramper.dev"
-
-export default {
-  domain,
-  api: `api.${domain}`,
-  widget: `widget.${domain}`,
-}
