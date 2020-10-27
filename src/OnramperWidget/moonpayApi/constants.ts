315ae669a38694d6064e1c075e23886faef4459a
corollari <github@albert.sh>
2020-10-27 09:43:31 +0100

    Add moonpay's prod keys

--- src/OnramperWidget/moonpayApi/constants.ts
+++ src/OnramperWidget/moonpayApi/constants.ts
@@ -1,4 +1,5 @@
-export const publishableApiKey = 'pk_test_PjABKr88VlgosyTueq3exrVnYYLd4ZB';
+export const publishableApiKey = process.env.REACT_APP_STAGE === 'prod' ?
+  'pk_live_MJouyH3Fe3BVaNOaHCt5SvBfJ5Mk7RvQ' : 'pk_test_PjABKr88VlgosyTueq3exrVnYYLd4ZB';
 export const moonpayBaseAPI = 'https://api.moonpay.io/v3';
 // This identifier is also referenced by copy in `infra/src/lambda-edge.ts`, any changes here should also be applied there
 export const identifier = 'Moonpay';
@@ -7,16 +8,8 @@ export const acceptedContentTypes = [
   'image/png',
   'application/pdf',
 ];
-export const baseCreditCardSandboxUrl =
-  process.env.REACT_APP_STAGE === 'prod'
-    ? 'https://sandbox.onramper.com'
-    : 'https://sandbox.onramper.dev';
-export const baseAPIUrl =
-    process.env.REACT_APP_STAGE === 'prod'
-      ? 'https://api.onramper.com'
-      : 'https://api.onramper.dev';
-export const baseUploadsUrl =
-    process.env.REACT_APP_STAGE === 'prod'
-      ? 'https://upload.onramper.com'
-      : 'https://upload.onramper.dev';
-  
+const domain = process.env.REACT_APP_STAGE === 'prod' ?
+  'onramper.com' : 'onramper.dev';
+export const baseCreditCardSandboxUrl = `https://sandbox.${domain}`
+export const baseAPIUrl = `https://api.${domain}`;
+export const baseUploadsUrl = `https://upload.${domain}`;
