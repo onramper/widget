81eee4f196301089c9b78f8c0b25583b7bb5d3b6
Salah Karim <salahkarimka@gmail.com>
2020-10-29 11:58:52 +0100

    Equal help text font-size in help pane overlay to input label font-size

--- src/OnramperWidget/steps/FormView/renderers/Help2FACreditCard.tsx
+++ src/OnramperWidget/steps/FormView/renderers/Help2FACreditCard.tsx
@@ -11,8 +11,8 @@ const Help2FACreditCard: React.FC<Help2FACreditCardType> = (props) => {
     return (
         <>
             <h4 className={stylesCommon['help-title']}>2FA Credit Card</h4>
-            <p>Enter the 6 digit Bank verification code. You can find this in your pending transactions.</p>
-            <img alt="Pending transaction screenshot" className={stylesCommon['help-img']} src={help_2fa_cc} />
+            <p className={stylesCommon['help-text']}>Enter the 6 digit Bank verification code. You can find this in your pending transactions.</p>
+            <img className={stylesCommon['help-img']} src={help_2fa_cc} alt="Pending transaction screenshot" />
         </>
     )
 }
