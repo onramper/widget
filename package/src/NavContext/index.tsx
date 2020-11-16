fe5acf431ce0f220c49f2aea7c9f61c16df3e168
Salah Karim <salahkarimka@gmail.com>
2020-11-16 13:23:21 +0100

    Small changes

--- package/src/NavContext/index.tsx
+++ package/src/NavContext/index.tsx
@@ -142,25 +142,27 @@ class NavContainer extends React.Component<{ home?: ScreenType }, NavigationStat
                   className={styles.floating}
                 />
               </CSSTransition>
-              <div className={`${styles["chat"]} ${this.state._isChatOpen ? styles["active"] : ''} ${!this.state.intro ? styles['shy'] : 'hi'}`} onClick={(e) => {
+              <div className={`${styles["chat"]} ${!this.state.intro ? styles['shy'] : 'hi'} ${this.state._isChatOpen ? styles["active"] : ''}`} onClick={(e) => {
                 e.preventDefault()
                 this.setState(prevState => ({ _isChatOpen: !prevState._isChatOpen }))
               }}>
-                <div className={styles["background"]}><span onClick={(e)=>e.preventDefault()} className={styles["background-text"]}>Do you need help?</span></div>
-                <svg className={styles["chat-bubble"]} width="46.875" height="46.875" viewBox="12.5 12.5 75 75">
-                  <g className={styles["bubble"]}>
-                    <path className={`${styles["line"]} ${styles["line1"]}`} d="M 30.7873,85.113394 30.7873,46.556405 C 30.7873,41.101961
+                <div className={`${styles['anim-container']}`}>
+                  <div className={styles["background"]}><span onClick={(e) => e.preventDefault()} className={styles["background-text"]}>Do you need help?</span></div>
+                  <svg className={styles["chat-bubble"]} width="46.875" height="46.875" viewBox="12 13.5 75 75">
+                    <g className={styles["bubble"]}>
+                      <path className={`${styles["line"]} ${styles["line1"]}`} d="M 30.7873,85.113394 30.7873,46.556405 C 30.7873,41.101961
           36.826342,35.342 40.898074,35.342 H 59.113981 C 63.73287,35.342
           69.29995,40.103201 69.29995,46.784744" />
-                    <path className={`${styles["line"]} ${styles["line2"]}`} d="M 13.461999,65.039335 H 58.028684 C
+                      <path className={`${styles["line"]} ${styles["line2"]}`} d="M 13.461999,65.039335 H 58.028684 C
             63.483128,65.039335
             69.243089,59.000293 69.243089,54.928561 V 45.605853 C
             69.243089,40.986964 65.02087,35.419884 58.339327,35.419884" />
-                  </g>
-                  <circle className={`${styles["circle"]} ${styles["circle1"]}`} r="1.9" cy="50.7" cx="42.5" />
-                  <circle className={`${styles["circle"]} ${styles["circle2"]}`} cx="49.9" cy="50.7" r="1.9" />
-                  <circle className={`${styles["circle"]} ${styles["circle"]}`} r="1.9" cy="50.7" cx="57.3" />
-                </svg>
+                    </g>
+                    <circle className={`${styles["circle"]} ${styles["circle1"]}`} r="1.9" cy="50.7" cx="42.5" />
+                    <circle className={`${styles["circle"]} ${styles["circle2"]}`} cx="49.9" cy="50.7" r="1.9" />
+                    <circle className={`${styles["circle"]} ${styles["circle"]}`} r="1.9" cy="50.7" cx="57.3" />
+                  </svg>
+                </div>
               </div>
               {/*               <button
                 onClick={(e) => {
