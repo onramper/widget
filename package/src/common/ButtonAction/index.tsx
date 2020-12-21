7bdc118c13d2ed686c6186e51c7b9a9819fb7a54
Salah Karim <salahkarimka@gmail.com>
2020-12-21 02:02:36 +0100

    Add prop to change button size

--- package/src/common/ButtonAction/index.tsx
+++ package/src/common/ButtonAction/index.tsx
@@ -5,11 +5,12 @@ type ButtonActionType = {
     onClick?: () => void,
     text: string,
     disabled?: boolean
+    size?: 'small' | 'large'
 }
 
 const ButtonAction: React.FC<ButtonActionType> = (props) => {
-    const { onClick, text, disabled = false } = props
-    return <button onClick={onClick} className={`${styles['button-action']} ${disabled ? styles['button-action--disabled'] : ''}`} disabled={disabled}>{text}</button>
+    const { onClick, text, disabled = false, size = 'large' } = props
+    return <button onClick={onClick} className={`${styles['button-action']} ${size === 'small' ? styles['button-action--small'] : ''} ${disabled ? styles['button-action--disabled'] : ''}`} disabled={disabled}>{text}</button>
 }
 
 export default ButtonAction
