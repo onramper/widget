f8243ed83ca5c70de669ee858a6b0355acbc1170
corollari <github@albert.sh>
2021-02-11 13:47:37 +0100

    Change prod api endpoint

--- package/src/ApiContext/api/constants.ts
+++ package/src/ApiContext/api/constants.ts
@@ -1,7 +1,7 @@
 export const documents = ['passport', 'nationalIdentityCard', 'residenceCard', 'driverLicense']
 
 export const BASE_API = process.env.STAGE === 'prod'
-    ? 'https://api.onramper.com'
+    ? 'https://onramper.tech'
     : 'https://test.onramper.tech';
 
 export const SANDBOX_HOSTNAME = process.env.STAGE === 'prod'
