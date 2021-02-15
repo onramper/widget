1d4d70b6d90fbc843d9eb335f4aa77fac308f66f
corollari <github@albert.sh>
2021-02-15 23:18:31 +0100

    Add sentry logging and update packages

--- iframe/src/index.tsx
+++ iframe/src/index.tsx
@@ -2,9 +2,20 @@ import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({
+  dsn: "https://44e974f8b9e24b448bc6f0b7c3ea2c12@o506512.ingest.sentry.io/5596296",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 
 if (process.env.REACT_APP_STAGE === 'demo') {
   const { worker } = require('./mocks')
