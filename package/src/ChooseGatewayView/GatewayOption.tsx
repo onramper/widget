6ed098c11e4fb22421708c162712b548acb40edb
unknown <salahkarimka@gmail.com>
2021-03-23 18:08:00 +0300

    Hide diff label when gateway not available

--- package/src/ChooseGatewayView/GatewayOption.tsx
+++ package/src/ChooseGatewayView/GatewayOption.tsx
@@ -148,7 +148,7 @@ const GatewayOption: React.FC<GateWayOptionProps> = (props) => {
                         {/* <CSSTransition in={isOpen && available} {...transitionPropsPrice}> */}
                         {
                             ((isOpen && available) && <span className={`${styles['receive-diff']}`} > {collected.amountInCrypto ? 'You pay:' : 'You receive:'}</span>)
-                            || ((!available || !isOpen) && !isAnOption && !(error?.type.match(/MIN|MAX/)) &&
+                            || (available && !isOpen && !isAnOption && !(error?.type.match(/MIN|MAX/)) &&
                                     /* available ? */
                                     <span ref={transitionRefs3} style={styleColorUpDownDiff} className={`${styles['receive-diff']} ${styles['receive-diff--diff']} ${`${isDiffPositive ? styles['diff--up'] : styles['diff--down']}`} `} > {`${diff2Render}%`}</span>
                                    /*  : <span ref={transitionRefs3} >Unavailable</span> */
