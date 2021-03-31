89c30f1d36463b40bfd96436e74c6060e02eb028
unknown <salahkarimka@gmail.com>
2021-03-31 19:15:44 +0300

    Fix button css

--- package/src/common/ButtonAction/index.tsx
+++ package/src/common/ButtonAction/index.tsx
@@ -1,23 +1,34 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React from "react";
+import styles from "./styles.module.css";
 
 type ButtonActionType = {
-    onClick?: () => void,
-    text: string,
-    disabled?: boolean
-    size?: 'small' | 'large'
-    className?: string
-}
+  onClick?: () => void;
+  text: string;
+  disabled?: boolean;
+  size?: "small" | "large";
+  className?: string;
+};
 
 const ButtonAction: React.FC<ButtonActionType> = (props) => {
-    const { onClick, text, disabled = false, size = 'large' } = props
-    return <button
-        onClick={onClick}
-        className={`${styles['button-action']} ${size === 'small' ? styles['button-action--small'] : ''} ${disabled ? styles['button-action--disabled'] : ''} ${props.className}`}
-        disabled={disabled}
-    >
-        {text}
-    </button>
-}
+  const {
+    onClick,
+    text,
+    disabled = false,
+    size = "large",
+    className = "",
+  } = props;
 
-export default ButtonAction
+  return (
+    <button
+      onClick={onClick}
+      className={`${styles["button-action"]} ${
+        size === "small" ? styles["button-action--small"] : ""
+      } ${disabled ? styles["button-action--disabled"] : ""} ${className}`}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default ButtonAction;
