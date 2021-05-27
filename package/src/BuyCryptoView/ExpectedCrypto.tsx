bd6cf2861ba344a536ef6722852035ab09a3145a
unknown <salahkarimka@gmail.com>
2021-05-28 01:11:29 +0200

    Fix lint

--- package/src/BuyCryptoView/ExpectedCrypto.tsx
+++ package/src/BuyCryptoView/ExpectedCrypto.tsx
@@ -1,50 +1,57 @@
-import React, { useContext, useEffect, useState } from 'react'
-import styles from './styles.module.css'
+import React, { useContext, useEffect, useState } from "react";
+import styles from "./styles.module.css";
 
-import { APIContext } from '../ApiContext'
+import { APIContext } from "../ApiContext";
 
 type ExpectedCryptoType = {
-    denom: string,
-    className?: string
-    isLoading: boolean
-    amountInCrypto?: boolean
-}
+  denom: string;
+  className?: string;
+  isLoading: boolean;
+  amountInCrypto?: boolean;
+};
 
 const ExpectedCrypto: React.FC<ExpectedCryptoType> = (props) => {
-    const { denom, className, isLoading, amountInCrypto = false } = props
+  const { denom, className, isLoading, amountInCrypto = false } = props;
 
-    const [expectedCrypto, setExpectedCrypto] = useState(0)
+  const [expectedCrypto, setExpectedCrypto] = useState(0);
 
-    const { data, inputInterface, collected } = useContext(APIContext);
-    const { handleInputChange } = inputInterface
+  const { data, inputInterface, collected } = useContext(APIContext);
+  const { handleInputChange } = inputInterface;
 
-    useEffect(() => {
-        let lowest = collected.amountInCrypto ? Number.POSITIVE_INFINITY : Number.NEGATIVE_INFINITY;
-        const comparator = (tmp: number, lowest: number) => collected.amountInCrypto ? tmp < lowest : tmp > lowest
-        let index = 0
-        let tmp: number;
-        const pricedRates = data.allRates.filter(item => item.available)
-        for (let i = pricedRates.length - 1; i >= 0; i--) {
-            tmp = pricedRates[i].receivedCrypto ?? 0;
-            if (comparator(tmp, lowest)) {
-                lowest = tmp;
-                index = i;
-            }
-        }
+  useEffect(() => {
+    let lowest = collected.amountInCrypto
+      ? Number.POSITIVE_INFINITY
+      : Number.NEGATIVE_INFINITY;
+    const comparator = (tmp: number, lowest: number) =>
+      collected.amountInCrypto ? tmp < lowest : tmp > lowest;
+    let index = 0;
+    let tmp: number;
+    const pricedRates = data.allRates.filter((item) => item.available);
+    for (let i = pricedRates.length - 1; i >= 0; i--) {
+      tmp = pricedRates[i].receivedCrypto ?? 0;
+      if (comparator(tmp, lowest)) {
+        lowest = tmp;
+        index = i;
+      }
+    }
 
-        setExpectedCrypto(pricedRates[index]?.receivedCrypto ?? 0)
-    }, [data.allRates, collected.amountInCrypto])
+    setExpectedCrypto(pricedRates[index]?.receivedCrypto ?? 0);
+  }, [data.allRates, collected.amountInCrypto]);
 
-    useEffect(() => {
-        handleInputChange('bestExpectedCrypto', expectedCrypto)
-    }, [expectedCrypto, handleInputChange])
+  useEffect(() => {
+    handleInputChange("bestExpectedCrypto", expectedCrypto);
+  }, [expectedCrypto, handleInputChange]);
 
-    return (
-        <div className={`${styles['expected-crypto']} ${className}`}>
-            <span className={styles['expected-crypto__amount']}>{isLoading ? 'Fetching best price...' : `${expectedCrypto} ${denom}`}</span>
-            <span className={styles['expected-crypto__info']}>{amountInCrypto ? 'Amount you pay' : 'You will receive'}</span>
-        </div>
-    )
-}
+  return (
+    <div className={`${styles["expected-crypto"]} ${className}`}>
+      <span className={styles["expected-crypto__amount"]}>
+        {isLoading ? "Fetching best price..." : `${expectedCrypto} ${denom}`}
+      </span>
+      <span className={styles["expected-crypto__info"]}>
+        {amountInCrypto ? "Amount you pay" : "You will receive"}
+      </span>
+    </div>
+  );
+};
 
-export default ExpectedCrypto
+export default ExpectedCrypto;
