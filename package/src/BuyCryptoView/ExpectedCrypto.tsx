08d689c47ba85e022d82864a26762886bb02fcaf
unknown <salahkarimka@gmail.com>
2021-05-28 01:11:02 +0200

    Fix expected crypto

--- package/src/BuyCryptoView/ExpectedCrypto.tsx
+++ package/src/BuyCryptoView/ExpectedCrypto.tsx
@@ -15,24 +15,25 @@ const ExpectedCrypto: React.FC<ExpectedCryptoType> = (props) => {
 
     const [expectedCrypto, setExpectedCrypto] = useState(0)
 
-    const { data, inputInterface } = useContext(APIContext);
+    const { data, inputInterface, collected } = useContext(APIContext);
     const { handleInputChange } = inputInterface
 
     useEffect(() => {
-        let lowest = Number.POSITIVE_INFINITY;
+        let lowest = collected.amountInCrypto ? Number.POSITIVE_INFINITY : Number.NEGATIVE_INFINITY;
+        const comparator = (tmp: number, lowest: number) => collected.amountInCrypto ? tmp < lowest : tmp > lowest
         let index = 0
         let tmp: number;
         const pricedRates = data.allRates.filter(item => item.available)
         for (let i = pricedRates.length - 1; i >= 0; i--) {
             tmp = pricedRates[i].receivedCrypto ?? 0;
-            if (tmp > lowest) {
+            if (comparator(tmp, lowest)) {
                 lowest = tmp;
                 index = i;
             }
         }
 
         setExpectedCrypto(pricedRates[index]?.receivedCrypto ?? 0)
-    }, [data.allRates])
+    }, [data.allRates, collected.amountInCrypto])
 
     useEffect(() => {
         handleInputChange('bestExpectedCrypto', expectedCrypto)
