5865827d7b4e4d557d178008cdb6135a9eb25a1e
Josh <mail@chukwuanu.com>
2021-06-24 03:34:16 +0100

    no empty string headers

--- package/src/ApiContext/api/index.ts
+++ package/src/ApiContext/api/index.ts
@@ -19,7 +19,9 @@ const sentryHub = new Hub(sentryClient);
 
 const authenticate = (pk: string) => {
     headers.set('Authorization', `Basic ${pk}`)
-    headers.set('X-Widget-Referer', document.referrer)
+    if (document.referrer) {
+        headers.set('X-Widget-Referer', document.referrer)
+    }
     sentryHub.addBreadcrumb({message:`Authenticated with API key '${pk}'`, category:'auth'})
 }
 
