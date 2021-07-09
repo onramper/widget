a0011934b5d790a3552a90256170054f1a4aa112
unknown <salahkarimka@gmail.com>
2021-07-09 20:35:53 +0200

    Hide burger menu only when is fullscreen iframe

--- package/src/steps/IframeView/index.tsx
+++ package/src/steps/IframeView/index.tsx
@@ -74,7 +74,7 @@ const IframeView: React.FC<{ nextStep: NextStep & { type: 'iframe' | "redirect" 
 
   return (
     <div className={styles.view}>
-      <Header title="Complete payment" backButton hideBurgerButton={true}/>
+      <Header title="Complete payment" hideBurgerButton={nextStep.type==='iframe'&&nextStep.fullscreen} backButton/>
       <BodyIframeView
         textInfo={nextStep.type === 'redirect' ? nextStep.hint : undefined}
         error={error}
