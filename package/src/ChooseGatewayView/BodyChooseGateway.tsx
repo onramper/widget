2f12b02e12c20d3517fe5d61e797337a79a66c83
unknown <salahkarimka@gmail.com>
2021-07-26 18:23:31 +0200

    Add notification message + change string

--- package/src/ChooseGatewayView/BodyChooseGateway.tsx
+++ package/src/ChooseGatewayView/BodyChooseGateway.tsx
@@ -1,51 +1,72 @@
-import React, { useContext } from 'react'
-import stylesCommon from '../styles.module.css'
+import React, { useContext } from "react";
+import stylesCommon from "../styles.module.css";
 
-import ButtonAction from '../common/ButtonAction'
+import ButtonAction from "../common/ButtonAction";
 
-import { APIContext, GatewayRateOption } from '../ApiContext'
-import InfoBox from '../common/InfoBox'
-import ErrorVisual from '../common/ErrorVisual'
-import RatesList from './RatesList'
+import { APIContext, GatewayRateOption } from "../ApiContext";
+import InfoBox from "../common/InfoBox";
+import ErrorVisual from "../common/ErrorVisual";
+import RatesList from "./RatesList";
 
 type BodyChooseGatewayType = {
-    onActionButton?: () => void,
-    onItemClick?: (index: number) => void,
-    ratesList: GatewayRateOption[]
-}
+  onActionButton?: () => void;
+  onItemClick?: (index: number) => void;
+  ratesList: GatewayRateOption[];
+};
 
 const BodyChooseGateway: React.FC<BodyChooseGatewayType> = (props) => {
-    const { data } = useContext(APIContext)
+  const { data } = useContext(APIContext);
 
-    const { onActionButton, onItemClick } = props
-    const { ratesList } = props
+  const { onActionButton, onItemClick } = props;
+  const { ratesList } = props;
 
-    const availableRates = ratesList.filter(el => el.available)
-    const unavailableRates = ratesList.filter(el => !el.available)
-    const hiddenRates = data.mappedHiddenByFiat
+  const availableRates = ratesList.filter((el) => el.available);
+  const unavailableRates = ratesList.filter((el) => !el.available);
+  const hiddenRates = data.mappedHiddenByFiat;
 
-    return (
-        <main className={stylesCommon.body}>
-            {ratesList.length > 0 ?
-                <div className={`${stylesCommon.body__child}`}>
-                    <RatesList availableRates={availableRates} unavailableRates={unavailableRates} hiddenRates={hiddenRates} onItemClick={onItemClick} />
-                </div>
-                : (
-                    <>
-                        <InfoBox in={true} type='info' className={`${stylesCommon.body__child}`}>
-                            No prices available at this time.
-                        </InfoBox>
-                        <div className={`${stylesCommon.body__child} ${stylesCommon.grow}`}>
-                            <ErrorVisual message="An error occurred while trying to connect to server. Please try again later." />
-                        </div>
-                    </>
-                )
-            }
-            <div className={`${stylesCommon.body__child} ${stylesCommon.grow}`}>
-                <ButtonAction onClick={onActionButton} text='Continue' disabled={availableRates.length < 1} />
-            </div>
-        </main>
-    )
-}
+  return (
+    <main className={stylesCommon.body}>
+      {ratesList.length > 0 ? (
+        <>
+          <InfoBox
+            in={true}
+            type="notification"
+            className={`${stylesCommon.body__child}`}
+          >
+            {"The displayed aounts are an approximate calculation of the crypto received after fees."}
+          </InfoBox>
+          <div className={`${stylesCommon.body__child}`}>
+            <RatesList
+              availableRates={availableRates}
+              unavailableRates={unavailableRates}
+              hiddenRates={hiddenRates}
+              onItemClick={onItemClick}
+            />
+          </div>
+        </>
+      ) : (
+        <>
+          <InfoBox
+            in={true}
+            type="info"
+            className={`${stylesCommon.body__child}`}
+          >
+            No prices available at this time.
+          </InfoBox>
+          <div className={`${stylesCommon.body__child} ${stylesCommon.grow}`}>
+            <ErrorVisual message="An error occurred while trying to connect to server. Please try again later." />
+          </div>
+        </>
+      )}
+      <div className={`${stylesCommon.body__child} ${stylesCommon.grow}`}>
+        <ButtonAction
+          onClick={onActionButton}
+          text="Continue"
+          disabled={availableRates.length < 1}
+        />
+      </div>
+    </main>
+  );
+};
 
-export default BodyChooseGateway
+export default BodyChooseGateway;
