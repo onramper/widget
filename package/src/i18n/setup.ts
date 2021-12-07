63040363a55017495ba36078cf733d5bdb09672f
Noorts <50550612+Noorts@users.noreply.github.com>
2021-12-07 09:59:15 +0100

    Add i18next

---
+++ package/src/i18n/setup.ts
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+/* i18n was set up using the following guide:
+ * https://dev.to/adrai/how-to-properly-internationalize-a-react-application-using-i18next-3hdb
+ *
+ * Language detector: https://github.com/i18next/i18next-browser-languageDetector
+ * i18next options: https://www.i18next.com/overview/configuration-options
+ */
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: process.env.NODE_ENV === 'development',
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {}
+  });
+
+export default i18n;
