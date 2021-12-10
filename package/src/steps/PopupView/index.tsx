d149e72c575451a4b9272c2922019a3d0d153c12
tudor-carare <tudor.carare@maxcode.net>
2021-12-10 12:13:03 +0200

    Add actionable-error step, popup step

--- package/src/steps/PopupView/index.tsx
+++ package/src/steps/PopupView/index.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import Header from "../../common/Header";
-import BodyIframeView from "./BodyIframeView";
+import PopupLauncherView from "./PopupView";
 import styles from "../../styles.module.css";
 /* import ErrorView from '../../common/ErrorView' */
 
@@ -8,22 +8,18 @@ import Step from "../Step";
 
 import { sentryHub, ApiError } from "../../ApiContext/api/index";
 import { NextStep } from "../../ApiContext";
-import {
-  finishCCTransaction,
-  baseCreditCardSandboxUrl,
-  checkTransaction,
-} from "@onramper/moonpay-adapter";
 
 import { NavContext } from "../../NavContext";
 
-const btcdirectFinishedOrigin = "https://btcdirect.sandbox.staging.onramper.tech";
 
-const IframeView: React.FC<{
-  nextStep: NextStep & { type: "iframe" | "redirect" };
+const PopupView: React.FC<{
+  nextStep: NextStep & { type: "popup" };
 }> = ({ nextStep }) => {
   const { replaceScreen, nextScreen } = useContext(NavContext);
   //const textInfo = 'Complete your payment. The form below is in a secure sandbox.'
   const [error, setError] = useState<string>();
+  const [processFinished, setProcessFinished] = useState<boolean>(false);
+  const [uploadFailed, setUploadFailed] = useState<boolean>(false);
   const [fatalError, setFatalError] = useState<string>();
 
   function reportError(message: string, fatal: boolean, eventData: any) {
@@ -45,51 +41,27 @@ const IframeView: React.FC<{
     const receiveMessage = async (event: MessageEvent) => {
       console.log("Received new event", event);
       console.log(event.origin);
-      if (![baseCreditCardSandboxUrl, btcdirectFinishedOrigin, event.origin].includes(event.origin)) return;
-      if (event.data.type === "INIT") {
-        setError(undefined);
-        setFatalError(undefined);
-        return;
-      }
+
       sentryHub.addBreadcrumb({
         message: `Received a postMessage from ${event.origin}`,
         data: event.data,
       });
 
-      if (event.data.gateway === "Moonpay") {
-        let returnedNextStep: any; //: NextStep;
-        try {
-          if (event.data.type === "card-completed") {
-            returnedNextStep = await finishCCTransaction(
-              event.data.transactionId,
-              event.data.ccTokenId
-            );
-          } else if (event.data.type === "2fa-completed") {
-            returnedNextStep = await checkTransaction(
-              event.data.moonpayTxId,
-              event.data.onramperTxId
-            );
-          } else {
-            throw new Error("Unexpected response received");
-          }
-          replaceScreen(<Step nextStep={returnedNextStep as NextStep} />);
-        } catch (e) {
-          if (event.data.type === "card-completed") {
-            (event.source as Window)?.postMessage("reset", "*");
-          } else if (event.data.type === "2fa-completed") {
-            /* nextScreen(<ErrorView type="TX" />) */
-            reportError(e.message, true, event.data);
-            return;
-          }
-          reportError(e.message, false, event.data);
+      if(event.data.gateway === "Coinify") {
+
+        const state = event.data.state;
+
+        setProcessFinished(true);
+        if (state === "submitted") {
+          replaceScreen(<Step nextStep={nextStep.nextStep as NextStep} />);
+        } else {
+          setUploadFailed(true);
         }
-      } else if (event.data.type) {
-        replaceScreen(<Step nextStep={event.data as NextStep} />);
       } else if (typeof event.data === "string") {
         reportError(event.data, false, event.data);
       } else {
         reportError(
-          "Unknow error. Please, contact help@onramper.com and provide the following info: " +
+          "Unknown error. Please, contact help@onramper.com and provide the following info: " +
             nextStep.url,
           false,
           event.data
@@ -104,25 +76,27 @@ const IframeView: React.FC<{
     <div className={styles.view}>
       <Header
         title={nextStep.humanName ?? "Complete payment"}
-        hideBurgerButton={nextStep.type === "iframe" && nextStep.fullscreen}
+        // hideBurgerButton={nextStep.type === "iframe" && nextStep.fullscreen}
         backButton
       />
-      <BodyIframeView
-        textInfo={nextStep.type === "redirect" ? nextStep.hint : undefined}
+      <PopupLauncherView
+        textInfo={""/*nextStep.message*/}
         error={error}
         fatalError={fatalError}
         features={
-          nextStep.type === "iframe" ? nextStep.neededFeatures : undefined
+          nextStep.neededFeatures !== null ? nextStep.neededFeatures : undefined
         }
         src={nextStep.url}
         type={nextStep.type}
+        failStep = {nextStep.failStep}
         onErrorDismissClick={(type) =>
           type === "FATAL" ? setFatalError(undefined) : setError(undefined)
         }
-        isFullScreen={nextStep.type === "iframe" ? nextStep.fullscreen : false}
+        uploadFailed={uploadFailed}
+        processFinished={processFinished}
       />
     </div>
   );
 };
 
-export default IframeView;
+export default PopupView;
