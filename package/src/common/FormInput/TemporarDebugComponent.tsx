9e4eb3fc81d83f7bc55ee67276ca92441a8486d7
Gabriel Diac <gabriel.diac@maxcode.net>
2021-12-15 18:29:08 +0200

    build the input upon the old one

--- package/src/common/FormInput/TemporarDebugComponent.tsx
+++ package/src/common/FormInput/TemporarDebugComponent.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import InputText from "../Input/InputText";
 import IconMetamask from "../../icons/metamask.png";
 import IconImToken from "../../icons/imtoken.png";
+import InputRedesign from "./Input/Input";
 
 const TemporarDebugComponent: React.FC = () => {
     const [value, setValue] = useState("");
@@ -10,6 +11,37 @@ const TemporarDebugComponent: React.FC = () => {
   return (
     <div style={{ flexGrow: 1 }}>
       <InputText
+        disabled={false}
+        symbol="This input symbol"
+        placeholder="input placeholder"
+        label={`This input label`}
+        className="some-class"
+        icon={IconImToken}
+        iconPosition="end"
+        symbolPosition="start"
+        onChange={(name: string, value: string) => {
+            setValue(value);
+        }}
+        value={value}
+        type={"text"}
+        name="cryptocurrencyAddress"
+        onIconClick={() => {
+            alert("icon click")
+        }}
+        error={value === "" ? "input error" : undefined}
+        hint={`this input hint`}
+        hintButton
+        onHintClick={() => {
+            alert("hint click")
+        }}
+        clickableIcon
+        maxLength={255}
+        info={"input info "}
+        iconTitle={`Icon title stuff`}
+        isRequired={true}
+      />
+
+      <InputRedesign
         disabled={false}
         symbol="This input symbol"
         placeholder="input placeholder"
