4d0b80a2119a98c4f57c7e589ceae994d4ae44e3
Gabriel Diac <gabriel.diac@maxcode.net>
2021-12-15 18:29:08 +0200

    build the input upon the old one

--- package/src/common/FormInput/Input/Input.models.ts
+++ package/src/common/FormInput/Input/Input.models.ts
@@ -1,6 +1,27 @@
+import type { DateType } from "./../../Input/DatePicker";
+
 export type InputProps = {
+    max?: string;
     disabled?: boolean;
+    symbol?: string;
     placeholder?: string;
     label?: string;
+    className?: string;
+    icon?: string;
+    iconPosition?: "start" | "end";
+    symbolPosition?: "start" | "end";
+    onChange?: (name: string, value: any, type?: string) => void;
+    value?: number | string | DateType;
+    type?: string;
+    name: string;
+    onIconClick?: (name: string, value: string, label: string) => void;
     error?: string;
+    hint?: string;
+    hintButton?: boolean;
+    onHintClick?: () => void;
+    clickableIcon?: boolean;
+    maxLength?: number;
+    info?: string;
+    iconTitle?: string;
+    isRequired?: boolean;
 }
