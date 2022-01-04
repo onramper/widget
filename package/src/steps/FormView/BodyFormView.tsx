7e1102af0fc6f903d5d72948cf7baa7df42a729f
Gabriel Diac <gabriel.diac@maxcode.net>
2022-01-04 14:02:12 +0200

    updated view terms and conditions

--- package/src/steps/FormView/BodyFormView.tsx
+++ package/src/steps/FormView/BodyFormView.tsx
@@ -281,9 +281,9 @@ const BodyFormView: React.FC<BodyFormViewType> = (props) => {
                             ))
                             || ((field.type === 'boolean' && field.name === 'termsOfUse') && (
                                 <label key={i} className={`${stylesCommon.body__child} ${styles.terms}`}>
-                                    <input type="checkbox" checked={collected[field.name] ?? false} name={field.name} onChange={(e) => onChange(e.currentTarget.name, e.currentTarget.checked, e.currentTarget.type)} />&nbsp;I accept {
+                                    <input type="checkbox" checked={collected[field.name] ?? false} name={field.name} onChange={(e) => onChange(e.currentTarget.name, e.currentTarget.checked, e.currentTarget.type)} />&nbsp;{t('formView.termsAgreement')} {
                                         field.terms?.map<React.ReactNode>((term, i) => <a key={i} href={term.url} target='_blank' rel="noopener noreferrer">{term.humanName}</a>)
-                                            .reduce((acc, actual, i, arr) => [acc, i === arr.length - 1 ? ' and ' : ', ', actual])
+                                            .reduce((acc, actual, i, arr) => [acc, i === arr.length - 1 ? ` ${t('misc.and')} ` : ', ', actual])
                                     }.</label>
                             ))
                             || ((field.type === 'boolean') && (field.name === 'areFundsFromLegalSources') && (
