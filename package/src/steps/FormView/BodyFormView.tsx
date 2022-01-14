f6039ed70e31b5b490f229e1d625eaaac1c91875
Tudor Carare <tudor.carare@maxcode.net>
2022-01-14 13:51:20 +0200

    Fix compilation issues

--- package/src/steps/FormView/BodyFormView.tsx
+++ package/src/steps/FormView/BodyFormView.tsx
@@ -286,7 +286,7 @@ const BodyFormView: React.FC<BodyFormViewType> = (props) => {
                                             .reduce((acc, actual, i, arr) => [acc, i === arr.length - 1 ? ` ${t('misc.and')} ` : ', ', actual])
                                     }.</label>
                             ))
-                            || ((field.type === 'boolean2') && (
+                            || ((field.type === 'boolean') && (field.name === 'areFundsFromLegalSources') && (
                                 <label key={i} className={`${stylesCommon.body__child} ${styles.terms}`}>
                                     <input type="checkbox" checked={collected[field.name] ?? false} name={field.name} onChange={(e) => onChange(e.currentTarget.name, e.currentTarget.checked, e.currentTarget.type)} />&nbsp;{
                                     field.humanName
