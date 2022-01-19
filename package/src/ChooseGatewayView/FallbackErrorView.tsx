0fc8c7796aeadb75c1f7749cbcaea36177ff642e
Gabriel Diac <gabriel.diac@maxcode.net>
2022-01-19 10:37:20 +0200

    removed message on empty list

--- package/src/ChooseGatewayView/FallbackErrorView.tsx
+++ package/src/ChooseGatewayView/FallbackErrorView.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import stylesCommon from "../styles.module.css";
 import InfoBox from "../common/InfoBox";
-import ErrorVisual from "../common/ErrorVisual";
 
 const FallbackErrorView: React.FC = () => (
   <main className={stylesCommon.body}>
     <InfoBox in={true} type="info" className={`${stylesCommon.body__child}`}>
       No prices available at this time.
     </InfoBox>
-    <div className={`${stylesCommon.body__child} ${stylesCommon.grow}`}>
-      <ErrorVisual message="An error occurred while trying to connect to server. Please try again later." />
-    </div>
   </main>
 );
 
