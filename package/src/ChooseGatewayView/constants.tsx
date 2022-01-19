a1591a5315df9ca411bb6ba3aa069dc8a89861c8
Gabriel Diac <gabriel.diac@maxcode.net>
2022-01-19 12:52:54 +0200

    clean up after gateway picker changes

--- package/src/ChooseGatewayView/constants.tsx
+++ package/src/ChooseGatewayView/constants.tsx
@@ -1,21 +1,9 @@
-import styles from './styles.module.css'
 import { BadgeType } from './ChooseGatewayView.models';
 import flashIcon from "./../icons/flash.svg";
 import percentageIcon from "./../icons/percentage.svg";
 import incognitoIcon from "./../icons/incognito.svg";
 
 export const cryptoAmountsWarning = "The displayed amounts are an approximate calculation of the crypto that you will receive after fees.";
-
-export const transitionPropsCollapse = {
-    timeout: 200,
-    classNames: {
-        enter: styles['collapse-enter'],
-        enterActive: styles['collapse-enter-active'],
-        exit: styles['collapse-exit'],
-        exitActive: styles['collapse-exit-active']
-    },
-    unmountOnExit: true
-}
 
 export const badgeItemMap = {
     [BadgeType.Fast]: {
