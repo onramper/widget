8d19927cd578cfc9f4c4e83a91a349fa3c3cbd78
Gabriel Diac <gabriel.diac@maxcode.net>
2022-01-31 22:34:23 +0200

    added screen and mock data; bypass process step condition without extending the boolean expression

--- package/src/ApiContext/api/index.ts
+++ package/src/ApiContext/api/index.ts
@@ -102,10 +102,9 @@ const tob64 = async (file: File): Promise<string | ArrayBuffer | null> => {
     });
 }
 
-const executeStep = async (step: NextStep, data: { [key: string]: any } | File, params?: ExecuteStepParams): Promise<NextStep> => {
-
-    if (step.type !== 'information' && step.type !== 'form' && step.type !== 'file' && step.type !== 'wait') throw new Error('Unexpected error: Invalid step end.')
-    if (step.url === undefined) throw new Error('Unexpected error: Invalid step end.')
+const executeStep = async (_step: NextStep, data: { [key: string]: any } | File, params?: ExecuteStepParams): Promise<NextStep> => {
+    const step = _step as (NextStep & {url?: string});
+    if (!step.url) throw new Error('Unexpected error: Invalid step end.');
 
     const isMoonpay = isMoonpayStep(step.url)
     const isFile = step.type === 'file'
