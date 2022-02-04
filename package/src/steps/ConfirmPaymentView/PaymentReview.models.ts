006010922034694db2f37dbb5969b9bbd5439632
Gabriel Diac <gabriel.diac@maxcode.net>
2022-02-04 15:34:57 +0200

    added consistent namming

--- package/src/steps/ConfirmPaymentView/PaymentReview.models.ts
+++
@@ -1,7 +0,0 @@
-import { NextStep } from "../../ApiContext";
-
-export type PaymentReviewProps = {
-  nextStep: NextStep & { type: "paymentReview" };
-  includeCryptoAddr?: boolean;
-  onButtonAction?: () => void;
-};
