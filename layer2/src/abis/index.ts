df0f2a311be1704a408d24851e7b508c66d3af71
Gerhard_Steenkamp <gerhard@linumlabs.com>
2022-02-07 16:40:16 +0200

    remove layer2 package from this repo

--- layer2/src/abis/index.ts
+++
@@ -1,4 +0,0 @@
-import ERC20 from './ERC20.json';
-import SwapRouter from './SwapRouter.json';
-
-export { ERC20, SwapRouter };
--- layer2/src/index.tsx
+++
@@ -1,6 +0,0 @@
-export * from './config/config';
-export * from './config/wallets';
-export * from './utils';
-export * from '@usedapp/core';
-export * from '@ethersproject/units';
-export * from '@web3-react/abstract-connector';
