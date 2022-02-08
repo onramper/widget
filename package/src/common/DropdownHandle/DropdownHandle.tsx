4ec919b58a8bb6ff0df49c8ae5b2cf7671c954d0
Gabriel Diac <gabriel.diac@maxcode.net>
2022-02-08 13:39:37 +0200

    adjusted existing dropdown handle -> moved bg color for fiat-icon from rule to its class; no arrow icon when disabled

--- package/src/common/DropdownHandle/DropdownHandle.tsx
+++ package/src/common/DropdownHandle/DropdownHandle.tsx
@@ -3,9 +3,9 @@ import styles from "./DropdownHandle.module.css";
 import arrowDownIcon from "./../../icons/arrow-down.svg";
 import { DropdownHandleProps } from "./DropdownHandle.models";
 
-const DropdownHandle: React.FC<DropdownHandleProps> = ({icon, value, className, iconClassname, onClick, disabled = false}) => {
+const DropdownHandle: React.FC<DropdownHandleProps> = ({icon, value, className, iconClassname, onClick, disabled = false }) => {
   return (
-    <div className={`${styles["handle-wrapper"]} ${className || ""} ${disabled ? styles["disabled"] : ""}`} onClick={() => !disabled && onClick(value)}>
+    <div className={`${styles["handle-wrapper"]} ${className || ""} ${disabled ? styles["disabled"] : ""}`} onClick={() => !disabled && onClick && onClick(value)}>
       <div className={`${styles["icon-handle-wrapper"]} ${iconClassname || ""}`}>
         {icon && (
           <img className={styles["icon-handle"]} src={icon} alt="icon-dropdown" />
@@ -13,7 +13,7 @@ const DropdownHandle: React.FC<DropdownHandleProps> = ({icon, value, className, 
       </div>
 
       <span className={styles["value-dropdown"]}>{value}</span>
-      <img className={styles["icon-down"]} src={arrowDownIcon} />
+      {!disabled && <img className={styles["icon-down"]} src={arrowDownIcon} />}
     </div>
   );
 };
