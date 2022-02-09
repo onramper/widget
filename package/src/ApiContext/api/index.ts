20730695016f46212a753b1b49f6fc4cc357c232
Josh <mail@chukwuanu.com>
2022-02-09 11:54:32 +0100

    fix moonpay nextStepType

--- package/src/ApiContext/api/index.ts
+++ package/src/ApiContext/api/index.ts
@@ -157,7 +157,7 @@ const executeStep = async (
   logRequest(step.url);
   const nextStepType = step.url.split("/")[5];
   let nextStep: FetchResponse;
-  if (isMoonpay && nextStepType !== "iframe") {
+  if (isMoonpay && nextStepType !== "redirect") {
     nextStep = await processMoonpayStep(step.url, { method, headers, body });
   } else {
     nextStep = await fetch(`${step.url}?${urlParams}`, {
