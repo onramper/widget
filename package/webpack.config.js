3e2efa7a2233c4e6e84f63921e4ed3880c18bb24
0xsalah <salah@onramper.com>
2022-02-10 16:42:39 +0100

    Set any react/* like package as external

--- package/webpack.config.js
+++ package/webpack.config.js
@@ -90,18 +90,21 @@ module.exports = env => ({
             extensions: ['ts', 'tsx'],
         })
     ],
-    externals: {
-        react: {
-            'commonjs': 'react',
-            'commonjs2': 'react',
-            'amd': 'react',
-            'root': 'React'
+    externals: [
+        {
+            react: {
+                'commonjs': 'react',
+                'commonjs2': 'react',
+                'amd': 'react',
+                'root': 'React'
+            },
+            'react-dom': {
+                'commonjs': 'react-dom',
+                'commonjs2': 'react-dom',
+                'amd': 'react-dom',
+                'root': 'ReactDOM'
+            },
         },
-        'react-dom': {
-            'commonjs': 'react-dom',
-            'commonjs2': 'react-dom',
-            'amd': 'react-dom',
-            'root': 'ReactDOM'
-        },
-    }
+        /^react\/.*$/i
+    ]
 })
