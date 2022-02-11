7de5853b5be8c3bc40ea56073c602d264ae3f364
Gerhard_Steenkamp <gerhard@linumlabs.com>
2022-02-11 12:45:36 +0200

    COMPILING SUCCESS

--- package/webpack.config.js
+++ package/webpack.config.js
@@ -93,16 +93,16 @@ module.exports = env => ({
     externals: [
         {
             react: {
-                'commonjs': 'react',
-                'commonjs2': 'react',
-                'amd': 'react',
-                'root': 'React'
+                commonjs: 'react',
+                commonjs2: 'react',
+                amd: 'react',
+                root: 'React'
             },
             'react-dom': {
-                'commonjs': 'react-dom',
-                'commonjs2': 'react-dom',
-                'amd': 'react-dom',
-                'root': 'ReactDOM'
+                commonjs: 'react-dom',
+                commonjs2: 'react-dom',
+                amd: 'react-dom',
+                root: 'ReactDOM'
             },
         },
         /^react\/.*$/i
