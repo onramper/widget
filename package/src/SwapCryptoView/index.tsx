32174615874b66073167de8cfaa0abcd3f2e8723
Gerhard_Steenkamp <gerhard@linumlabs.com>
2022-02-16 22:32:13 +0200

    add progress header

--- package/src/SwapCryptoView/index.tsx
+++ package/src/SwapCryptoView/index.tsx
@@ -1,8 +1,34 @@
-import PlayGround from "./PlayGround";
-import React from "react";
+import styles from "../styles.module.css";
+import React, { useState } from "react";
+import ProgressHeader from "../common/Header/ProgressHeader/ProgressHeader";
 
 const SwapCryptoView = () => {
-  return <PlayGround />;
+  const steps = ["one", "two", "three", "four", "five"];
+  const [progress, setProgress] = useState(0);
+
+  const handleBack = () => {
+    if (progress > 0) {
+      setProgress(progress - 1);
+    }
+  };
+
+  const handleForward = () => {
+    if (progress < steps.length) {
+      setProgress(progress + 1);
+    }
+  };
+
+  return (
+    <div className={styles.view}>
+      <ProgressHeader
+        noSeparator
+        useBackButton
+        percentage={(100 / steps.length) * progress}
+      />
+      <button onClick={handleBack}>back</button>
+      <button onClick={handleForward}>forward</button>
+    </div>
+  );
 };
 
 export default SwapCryptoView;
