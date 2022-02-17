3539136e5117adf1b45d04459379e6ec05723c1a
Gabriel Diac <gabriel.diac@maxcode.net>
2022-02-17 15:33:47 +0200

    added navigation to swap screen

--- package/src/steps/SwapOverviewView/SwapOverviewView.tsx
+++ package/src/steps/SwapOverviewView/SwapOverviewView.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { SwapOverviewViewProps } from "./SwapOverviewView.models";
 import commonClasses from "../../styles.module.css";
+import ProgressHeader from "../../common/Header/ProgressHeader/ProgressHeader";
+import { NextStep } from "../../ApiContext";
 
-const SwapOverviewView = ({ nextStep }: SwapOverviewViewProps) => {
+const SwapOverviewView: React.FC<{
+  nextStep: NextStep & { type: "transactionOverview" };
+}> = ({ nextStep }) => {
+  const steps = ["one", "two", "three", "four", "five"];
+  const [progress, setProgress] = useState(0);
+
+  const handleBack = () => {
+    if (progress > 0) {
+      setProgress(progress - 1);
+    }
+  };
+
+  const handleForward = () => {
+    if (progress < steps.length) {
+      setProgress(progress + 1);
+    }
+  };
+
   return (
     <div className={commonClasses.view}>
-      <p>step 1</p>
+      <ProgressHeader
+        noSeparator
+        useBackButton
+        percentage={(100 / steps.length) * progress}
+      />
+      <main className={`${commonClasses.body}`}>SwapOverviewView</main>
     </div>
   );
 };
