751773221fd79675848a6064aeecb700b4b11b26
gabrieldiac11 <93923374+gabrieldiac11@users.noreply.github.com>
2022-02-17 12:42:12 +0200

    Features/616/621 destination wallet screen (#62)
    * added reusable wallet item with idle state
    
    * added selected state
    
    * styled wallet item's input; added a mock promise; extended input to support svg for icon
    
    * added transition between edit state and idle state in wallet item
    
    * fixed issue with elipsys width
    
    * moved wallet input to its own component to be reused later
    
    * added inner scroll container; added more items to mock data; separated metamask from other wallets; added delete function
    
    * addded synced transition idle-input states, so as to leave the input wrapper non-absolute and the error message to grow the wallet-item height
    
    * added 'enter additional wallet address' input
    
    * added autofocus when toggling input for wallet item
    
    * added continue disabled/enabled
    
    * pass the correct autofocus from wallet input
    
    * solved box shadow cutting from overflow auto
    
    * removed temp auto-redirect to the destination wallet; update todo comment;

--- package/src/ApiContext/api/types/nextStep.ts
+++ package/src/ApiContext/api/types/nextStep.ts
@@ -123,9 +123,20 @@ export type PaymentReviewStep = {
     data: PayamentReviewDataItem[]
 }
 
+export type WalletItemData = {
+    walletAddress: string;
+    accountName: string;
+    id: string;
+    balance: number;
+    icon?: string;
+    isConnected?: boolean;
+}
 export type DestinationWalletStep = {
     type: "destinationWallet"
     url: string;
+    data: WalletItemData[];
+    cryptoName: string;
+    selectedWalletId?: string;
 }
 
 type NextStep =
--- package/src/steps/DestinationWalletView/DestinationWalletView.tsx
+++ package/src/steps/DestinationWalletView/DestinationWalletView.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useState } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import { DestinationWalletViewProps } from "./DestinationWalletView.models";
 import commonClasses from "../../styles.module.css";
 import classes from "./DestinationWalletView.module.css";
@@ -9,22 +9,94 @@ import InfoBox from "../../common/InfoBox";
 import ButtonAction from "../../common/ButtonAction";
 import Footer from "../../common/Footer";
 import ErrorView from "../../common/ErrorView";
+import WalletItem from "./WalletItem/WalletItem";
+import WalletInput from "./WalletInput/WalletInput";
+import { WalletItemData } from "../../ApiContext/api/types/nextStep";
 
-const ComponentName: React.FC<DestinationWalletViewProps> = ({ nextStep }) => {
-  const [isLoading, setIsLoading] = useState(false);
+const DestinationWalletView: React.FC<DestinationWalletViewProps> = ({
+  nextStep,
+}) => {
   const [errorMessage, setErrorMessage] = useState<string>();
+  const [defaultWallets] = useState(
+    nextStep.data.filter((i) => i.id === "metamask")
+  );
+  const [wallets, setWallets] = useState(
+    nextStep.data.filter((i) => i.id !== "metamask")
+  );
+  const [walletId, setWalletId] = useState(nextStep.selectedWalletId);
+  const [isOverflowing, setIsOverflowing] = useState(false);
+  const [newWalletAddress, setNewWalletAddress] = useState("");
+  const [newWalletError, setNewWalletAddressError] = useState<
+    string | undefined
+  >();
+
+  const walletsWrapperRef = React.useRef<HTMLDivElement>(null);
 
   const { nextScreen, backScreen } = useContext(NavContext);
 
+  const onSubmitAddress = useCallback(
+    (address: string, walletId: string) => {
+      return new Promise<void>((resolve, reject) => {
+        if (!address) {
+          return reject(new Error("Value cannot be empty."));
+        }
+
+        setTimeout(() => {
+          
+          if (address === "error") {
+            return reject(new Error("Incorect address."));
+          }
+
+          setWallets(
+            wallets.map((i) =>
+              i.id === walletId ? { ...i, walletAddress: address } : i
+            )
+          );
+          resolve();
+        }, 200);
+      });
+    },
+    [wallets]
+  );
+
+  const onAddNewWallet = useCallback(async () => {
+    setNewWalletAddressError(undefined);
+
+    const promise = new Promise<WalletItemData>((resolve, reject) => {
+      setTimeout(() => {
+        if (!newWalletAddress) {
+          return reject(new Error("Value cannot be empty."));
+        }
+        
+        if (newWalletAddress === "error") {
+          return reject(new Error("Incorect address."));
+        }
+
+        const newWallet = {
+          walletAddress: newWalletAddress,
+          accountName: `Account ${wallets.length + 1}`,
+          id: `account${wallets.length + 1}`,
+          balance: (1000 * Math.random()) | 0,
+        };
+        resolve(newWallet);
+      }, 200);
+    });
+
+    try {
+      const wallet = await promise;
+      setNewWalletAddress("");
+      setWallets([wallet, ...wallets]);
+    } catch (_err) {
+      const error = _err as Error;
+      setNewWalletAddressError(error.message);
+    }
+  }, [newWalletAddress, wallets]);
+
   const onActionButton = useCallback(async () => {
-    setIsLoading(true);
     setErrorMessage(undefined);
 
     try {
-      //   TODO: next step call
-      //   const newNextStep = await apiInterface.executeStep(nextStep, {});
-      //   nextScreen(<Step nextStep={newNextStep} />);
-
+      //   TODO: update backend part using nextStep.url and then if ok, go back
       backScreen();
     } catch (_error) {
       const error = _error as { fatal: any; message: string };
@@ -34,10 +106,29 @@ const ComponentName: React.FC<DestinationWalletViewProps> = ({ nextStep }) => {
       }
       setErrorMessage(error.message);
     }
-    setIsLoading(false);
   }, [backScreen, nextScreen]);
 
-  const isFilled = false;
+  const onDeleteWallet = useCallback(
+    (id: string) => {
+      setWallets(wallets.filter((i) => i.id !== id));
+    },
+    [wallets]
+  );
+
+  const isContinueDisabled = useCallback(() => {
+    return ![...defaultWallets, ...wallets].some(i => i.id === walletId);
+  }, [defaultWallets, walletId, wallets]);
+
+  useEffect(() => {
+    if (!walletsWrapperRef.current) {
+      return;
+    }
+    const value =
+      walletsWrapperRef.current?.scrollHeight >
+      walletsWrapperRef.current?.clientHeight;
+    walletsWrapperRef.current.setAttribute("is-overflowing", String(value));
+    setIsOverflowing(value);
+  }, [defaultWallets.length, wallets.length]);
 
   return (
     <div className={commonClasses.view}>
@@ -63,13 +154,71 @@ const ComponentName: React.FC<DestinationWalletViewProps> = ({ nextStep }) => {
           <span> {errorMessage} </span>
         </InfoBox>
 
+        <div className={classes["main-content"]}>
+          {defaultWallets.length > 0 && (
+            <div className={classes["default-wallets-wrapper"]}>
+              {defaultWallets.map((wallet, index) => {
+                return (
+                  <WalletItem
+                    key={index}
+                    label={wallet.accountName}
+                    title={wallet.walletAddress}
+                    address={wallet.walletAddress}
+                    info={`Balance: ${wallet.balance} ${nextStep.cryptoName}`}
+                    isChecked={wallet.id === walletId}
+                    icon={wallet.icon}
+                    onCheck={() => setWalletId(wallet.id)}
+                    isConnected={wallet.isConnected}
+                  />
+                );
+              })}
+              <div className={classes["separator"]}></div>
+            </div>
+          )}
+
+          <div
+            ref={walletsWrapperRef}
+            is-overflowing={String(isOverflowing)}
+            className={`${commonClasses["sec-scrollbar"]} ${classes["wallets-wrapper"]}`}
+          >
+            <div className={classes["input-wrapper"]}>
+              <div className={classes["input-label"]}>
+                Enter an additional wallet address
+              </div>
+              <WalletInput
+                onSubmit={() => onAddNewWallet()}
+                errorMessage={newWalletError}
+                value={newWalletAddress}
+                onChange={setNewWalletAddress}
+              />
+            </div>
+
+            {wallets.map((wallet, index) => {
+              return (
+                <WalletItem
+                  key={index}
+                  label={wallet.accountName}
+                  title={wallet.walletAddress}
+                  address={wallet.walletAddress}
+                  info={`Balance: ${wallet.balance} ${nextStep.cryptoName}`}
+                  isChecked={wallet.id === walletId}
+                  icon={wallet.icon}
+                  onCheck={() => setWalletId(wallet.id)}
+                  onSubmitAddress={(value) => onSubmitAddress(value, wallet.id)}
+                  onDelete={() => onDeleteWallet(wallet.id)}
+                />
+              );
+            })}
+          </div>
+        </div>
+
         <div
-          className={`${commonClasses["body-form-child"]} ${commonClasses["grow-col"]}`}
+          className={`${commonClasses["body-form-child"]} ${classes["bottom-container"]}`}
         >
           <ButtonAction
             onClick={onActionButton}
-            text={isLoading ? "Sending..." : "Continue"}
-            disabled={!isFilled || isLoading}
+            disabled={isContinueDisabled()}
+            text={"Continue"}
           />
           <Footer />
         </div>
@@ -78,4 +227,4 @@ const ComponentName: React.FC<DestinationWalletViewProps> = ({ nextStep }) => {
   );
 };
 
-export default ComponentName;
+export default DestinationWalletView;
