3c53c772c10efcfeeddb487a8f584b84264af28a
Gabriel Diac <gabriel.diac@maxcode.net>
2022-02-17 16:35:17 +0200

    added elements and sizes

--- package/src/SwapCryptoView/TransactionErrorOverlay/TransactionErrorOverlay.tsx
+++ package/src/SwapCryptoView/TransactionErrorOverlay/TransactionErrorOverlay.tsx
@@ -3,10 +3,13 @@ import { TransactionErrorOverlayProps } from "./TransactionErrorOverlay.models";
 import commonClasses from "../../styles.module.css";
 import classes from "./TransactionErrorOverlay.module.css";
 import { ReactComponent as SettingsIcon } from "./../../icons/settings.svg";
+import { ReactComponent as CloseIcon } from "./../../icons/settings.svg";
 import { NavContext } from "../../NavContext";
 import ButtonAction from "../../common/ButtonAction";
 
-const TransactionErrorOverlay: React.FC<TransactionErrorOverlayProps> = () => {
+const TransactionErrorOverlay: React.FC<TransactionErrorOverlayProps> = (
+  props
+) => {
   const { backScreen } = useContext(NavContext);
 
   const onDismiss = useCallback(() => {
@@ -15,9 +18,21 @@ const TransactionErrorOverlay: React.FC<TransactionErrorOverlayProps> = () => {
 
   return (
     <div className={`${commonClasses.view} ${classes["view"]}`}>
-      <main className={`${commonClasses.body} ${classes["wrapper"]}`}>
-        <div className={`${classes["inner-wrapper"]}`}>
-          <SettingsIcon className={classes["setting-icon"]} />
+      <nav>
+        <div> Failed </div>
+        <CloseIcon className={classes["close-icon"]} />
+      </nav>
+
+      <main className={commonClasses.body}>
+        <div className={classes["wrapper"]}>
+          <div className={classes["settings-wrapper"]}>
+            <SettingsIcon className={classes["setting-icon"]} />
+          </div>
+          
+          <div className={classes["title"]}>Transaction Failed</div>
+          <div className={classes["text-alert"]}>{props.textAlert}</div>
+          <div className={classes["description"]}>{props.description}</div>
+
           <ButtonAction
             className={classes["dismiss-btn"]}
             onClick={onDismiss}
