fc5bacad3f848c504abdf89be4d51d3713feb9e1
Dinisha Tennakoon <dinisha@pastbook.com>
2022-03-15 12:55:43 +0530

    ONRMP-923 Getting the next step from the API

--- package/src/steps/PickOptionView/index.tsx
+++ package/src/steps/PickOptionView/index.tsx
@@ -1,30 +1,42 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import Step from "../Step";
-import { NextStep } from "../../ApiContext";
+import { APIContext, NextStep } from "../../ApiContext";
 import { NavContext } from "../../NavContext";
 import Heading from "../../common/Heading/Heading";
 import OptionsView from "./OptionsView";
 import ButtonAction from "../../common/ButtonAction";
 import Footer from "../../common/Footer";
+import ProgressHeader from "../../common/Header/ProgressHeader/ProgressHeader";
+import ErrorView from "../../common/ErrorView";
 
 import commonStyles from "../../styles.module.css";
 import styles from "./styles.module.css";
-import ProgressHeader from "../../common/Header/ProgressHeader/ProgressHeader";
 
 const PickOptionView: React.FC<{
   nextStep: NextStep & { type: "pickOne" };
 }> = ({ nextStep }) => {
   const { t } = useTranslation();
   const { nextScreen } = useContext(NavContext);
+  const { apiInterface } = useContext(APIContext);
 
   const nextStepOptions = nextStep.options || [];
 
   const [selectedOption, setSelectedOption] = useState(nextStepOptions[0]);
   const [isFilled, setIsFilled] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleButtonAction = async () => {
-    nextScreen(<Step nextStep={selectedOption} />);
+    setIsLoading(true);
+    try {
+      const newNextStep = await apiInterface.executeStep(selectedOption, {});
+      nextScreen(<Step nextStep={newNextStep} />);
+    } catch (_error) {
+      const error = _error as { fatal: any; message: string };
+      if (error.fatal) {
+        nextScreen(<ErrorView />);
+      }
+    }
   };
 
   const handleOptionChange = (i: number) => {
@@ -61,7 +73,7 @@ const PickOptionView: React.FC<{
           <ButtonAction
             onClick={handleButtonAction}
             text={t("button.continue")}
-            disabled={!isFilled}
+            disabled={!isFilled && isLoading}
           />
           <Footer />
         </div>
