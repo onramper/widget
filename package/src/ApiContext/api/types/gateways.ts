7a5b4d7f0bf790b1b771dc739a384a4df28b9701
Gerhard_Steenkamp <gerhard@linumlabs.com>
2022-04-29 12:44:40 +0200

    add some fields to Currency interface

--- package/src/ApiContext/api/types/gateways.ts
+++ package/src/ApiContext/api/types/gateways.ts
@@ -1,43 +1,40 @@
 interface GatewaysResponse {
-    gateways: IGatewayItem[]
-    localization: {
-        country: string,
-        state: string | null,
-        currency: string
-    }
-    icons?: {
-        [key: string]: IconGatewaysResponse
-    }
-    defaultAmounts?: {
-        [key: string]: number
-    }
+  gateways: IGatewayItem[];
+  localization: {
+    country: string;
+    state: string | null;
+    currency: string;
+  };
+  icons?: {
+    [key: string]: IconGatewaysResponse;
+  };
+  defaultAmounts?: {
+    [key: string]: number;
+  };
 }
 
 interface Currency {
-    code: string // display only
-    id: string // internal id e.g. bnb-bep20
-    precision: number
-    network?: string
-    displayName?: string
-    supportsAddressTag?: boolean
+  code: string; // display only
+  id: string; // internal id e.g. bnb-bep20
+  precision: number;
+  network?: string;
+  displayName?: string;
+  supportsAddressTag?: boolean;
+  chainId?: number; // added for L2 tokens
+  tokenAddress?: string; // added for L2 tokens
 }
 
 interface IconGatewaysResponse {
-    name: string
-    icon: string
-    symbol?: string
+  name: string;
+  icon: string;
+  symbol?: string;
 }
 
 interface IGatewayItem {
-    identifier: string
-    paymentMethods: string[]
-    fiatCurrencies: Currency[]
-    cryptoCurrencies: Currency[]
+  identifier: string;
+  paymentMethods: string[];
+  fiatCurrencies: Currency[];
+  cryptoCurrencies: Currency[];
 }
 
-export type {
-    GatewaysResponse,
-    IconGatewaysResponse,
-    IGatewayItem,
-    Currency
-}
+export type { GatewaysResponse, IconGatewaysResponse, IGatewayItem, Currency };
