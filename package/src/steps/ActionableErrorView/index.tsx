8db3a940fafc597de18277a774a988f7043c60d6
Gabriel Diac <gabriel.diac@maxcode.net>
2022-05-03 15:10:11 +0300

    Enhacement coinify payment iframe - PR -> https://github.com/onramper/widget/pull/74

---
+++ package/src/steps/ActionableErrorView/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Header from "../../common/Header";
+import styles from "../../styles.module.css";
+import { NextStep} from "../../ApiContext";
+import BodyActionableErrorView from "./BodyActionableErrorView";
+
+type ActionableErrorProps = {
+  nextStep: Partial<NextStep> & { type: "actionable-error" };
+};
+
+const ActionableErrorView: React.FC<ActionableErrorProps> = (props) => {
+  return (
+    <div className={styles.view}>
+      <Header
+        title={
+          props.nextStep.humanName ?? ""
+        }
+        backButton={false}
+      />
+      {(
+        <BodyActionableErrorView
+          title = {props.nextStep.title}
+          message = {props.nextStep.message}
+          fatal = {props.nextStep.fatal}
+          step = {props.nextStep.nextStep}
+          optionalUrl = {props.nextStep.optionalUrl}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ActionableErrorView;
