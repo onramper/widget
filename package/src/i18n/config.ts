9f3290f8257091b3dae93fb36c2f1929c9d108ee
Noorts <50550612+Noorts@users.noreply.github.com>
2022-05-18 10:48:40 +0200

    Fix i18n translations

--- package/src/i18n/config.ts
+++ package/src/i18n/config.ts
@@ -1,12 +1,14 @@
 import { defaultLanguage } from './../ApiContext/utils/languages';
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
+// import LanguageDetector from "i18next-browser-languagedetector";
+import { resources } from './translations';
+// import Backend from "i18next-http-backend";
+
 
 i18n
-  .use(Backend)
-  .use(LanguageDetector)
+  // .use(Backend)
+  // .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     debug: false,
@@ -14,11 +16,12 @@ i18n
     interpolation: {
       escapeValue: false,
     },
-    backend: {
-      // for all available options read the backend's repository readme file /locales/{{lng}}/{{ns}}.json
-      loadPath: "/locales/{{lng}}/{{ns}}.json",
-      crossDomain: false,
-    },
+    // backend: {
+    //   // for all available options read the backend's repository readme file /locales/{{lng}}/{{ns}}.json
+    //   loadPath: "/locales/{{lng}}/{{ns}}.json",
+    //   crossDomain: false,
+    // },
+    resources: resources
   });
 
 export default i18n;
