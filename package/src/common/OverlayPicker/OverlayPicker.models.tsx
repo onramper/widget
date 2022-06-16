d4b76859e60bf8bfa91c9057a1e98cb3d1c986ff
Gabriel Diac <gabriel.diac@maxcode.net>
2022-06-16 10:15:56 +0300

    added close dropdown gtm event

--- package/src/common/OverlayPicker/OverlayPicker.models.tsx
+++ package/src/common/OverlayPicker/OverlayPicker.models.tsx
@@ -8,4 +8,5 @@ export type OverlayPickerProps = {
   indexSelected?: number;
   searchable?: boolean;
   onItemClick?: (listName: string, index: number, item: ItemType) => void;
+  onCloseBtn?: () => void;
 };
