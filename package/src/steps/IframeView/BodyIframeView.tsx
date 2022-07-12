b0ba9631c9b5989ccc77bdb4dc5358e89ccc384c
chamith-onramper <106953947+chamith-onramper@users.noreply.github.com>
2022-07-12 18:57:34 +0530

    initial test added

--- package/src/steps/IframeView/BodyIframeView.tsx
+++ package/src/steps/IframeView/BodyIframeView.tsx
@@ -70,6 +70,7 @@ const BodyIframeView: React.FC<BodyIframeViewType> = (props) => {
     hostname === getHostname(selectedGateway?.nextStep.url);
 
   const gtmPayloadRef = useRef(props.gtmPayload);
+  const windowObjectReference: any = useRef(null);
 
   const restartWidget = () => {
     apiInterface.clearErrors();
@@ -90,30 +91,69 @@ const BodyIframeView: React.FC<BodyIframeViewType> = (props) => {
     props.fatalError,
   ]);
 
-  const redirect = useCallback(async (url: string) => {
-    setAutoRedirect(true);
-    //try to open popup
-    const windowObjectReference = window.open(
-      url,
-      "_blank",
-      "height=595,width=440,scrollbars=yes,left=0"
-    ); //todo: add config
-    //if opened -> all is ok
-    if (windowObjectReference) {
-      triggerGTMEvent(gtmPayloadRef.current);
+  const retryRedirect = (url: string) => {
+    let count = 3;
+    const interval = setInterval(() => {
+      if (!windowObjectReference.current && count > 0) {
+        count--;
+        windowObjectReference.current = window.open(
+          url,
+          "_blank",
+          "height=595,width=440,scrollbars=yes,left=0,popup=yes"
+        );
+        setAutoRedirect(true);
+      }
+      clearInterval(interval);
+    });
+  };
 
-      const interval = 250;
-      const times2Count = (1000 * 60) / interval;
-      let count = 0;
-      const checkIfClosed = setInterval(() => {
-        if (windowObjectReference.closed || count > times2Count) {
-          setUserClosedPopup(true);
-          clearInterval(checkIfClosed);
-        }
-        count++;
-      }, interval);
+  const redirect = useCallback(async (url: string) => {
+    const interval = 250;
+    const times2Count = (1000 * 60) / interval;
+    let count = 0;
+    const checkIfClosed = setInterval(() => {
+      if (windowObjectReference.current.closed || count > times2Count) {
+        setUserClosedPopup(true);
+        clearInterval(checkIfClosed);
+      }
+      count++;
+    }, interval);
+
+    if (windowObjectReference.current) {
+      windowObjectReference.current.focus();
+      triggerGTMEvent(gtmPayloadRef.current);
       return;
+    } else {
+      retryRedirect(url);
+      // setAutoRedirect(true);
+      // windowObjectReference.current = window.open(
+      //   url,
+      //   "_blank",
+      //   "height=595,width=440,scrollbars=yes,left=0,popup=yes"
+      // );
     }
+    //try to open popup
+    //  windowObjectReference.current = window.open(
+    //   url,
+    //   "_blank",
+    //   "height=595,width=440,scrollbars=yes,left=0,popup=yes"
+    // ); //todo: add config
+    //if opened -> all is ok
+    // if (windowObjectReference.current) {
+    //   triggerGTMEvent(gtmPayloadRef.current);
+
+    //   const interval = 250;
+    //   const times2Count = (1000 * 60) / interval;
+    //   let count = 0;
+    //   const checkIfClosed = setInterval(() => {
+    //     if (windowObjectReference.current.closed || count > times2Count) {
+    //       setUserClosedPopup(true);
+    //       clearInterval(checkIfClosed);
+    //     }
+    //     count++;
+    //   }, interval);
+    //   return;
+    // }
     //if not opened -> warn user about popup blocked + ask user for click a button
     setAutoRedirect(false);
   }, []);
