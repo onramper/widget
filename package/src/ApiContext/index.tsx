98ea97fbb4428b04ab1944181b386abcf111788d
chamith-onramper <106953947+chamith-onramper@users.noreply.github.com>
2022-09-22 19:09:39 +0530

    Revert "feat: WT-290 add crypto, fiat offramp params (#244)" (#247)
    This reverts commit 4ef6537e955b9c2b73e253b39fe6093c8e072e0c.

--- package/src/ApiContext/index.tsx
+++ package/src/ApiContext/index.tsx
@@ -38,6 +38,7 @@ import type {
   InfoDepositBankAccount,
 } from "./api/types/nextStep";
 
+
 import { NextStepError } from "./api";
 import type { Filters, Transaction } from "./api";
 import phoneCodes from "./utils/phoneCodes";
@@ -141,8 +142,6 @@ const APIProvider: React.FC<APIProviderType> = (props) => {
       skipTransactionScreen: props.skipTransactionScreen,
       transaction: props.transaction,
       initScreen: props.initScreen,
-      defaultCrypto: defaultCrypto,
-      defaultFiat: defaultFiat,
     };
   }, [
     defaultAmount,
@@ -162,8 +161,6 @@ const APIProvider: React.FC<APIProviderType> = (props) => {
     props.skipTransactionScreen,
     props.transaction,
     props.initScreen,
-    defaultCrypto,
-    defaultFiat,
   ]);
 
   const iniState: StateType = {
