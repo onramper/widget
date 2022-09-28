33de126fc4cb5006c65c7d5ccd267926c74d3795
chamith-onramper <106953947+chamith-onramper@users.noreply.github.com>
2022-09-28 10:15:33 +0530

    feat: WT-290 add crypto, fiat offramp params (#249)

--- package/src/ApiContext/index.tsx
+++ package/src/ApiContext/index.tsx
@@ -38,7 +38,6 @@ import type {
   InfoDepositBankAccount,
 } from "./api/types/nextStep";
 
-
 import { NextStepError } from "./api";
 import type { Filters, Transaction } from "./api";
 import phoneCodes from "./utils/phoneCodes";
@@ -143,6 +142,8 @@ const APIProvider: React.FC<APIProviderType> = (props) => {
       skipTransactionScreen: props.skipTransactionScreen,
       transaction: props.transaction,
       initScreen: props.initScreen,
+      defaultCrypto: defaultCrypto,
+      defaultFiat: defaultFiat,
     };
   }, [
     defaultAddrs,
@@ -161,6 +162,8 @@ const APIProvider: React.FC<APIProviderType> = (props) => {
     props.skipTransactionScreen,
     props.transaction,
     props.initScreen,
+    defaultCrypto,
+    defaultFiat,
   ]);
 
   const iniState: StateType = {
