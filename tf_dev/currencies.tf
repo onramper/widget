80cf68ef3c4712284bbb54a29ade7df82932c648
Dananjana Kuruppuachchi <dananjana@onramper.com>
2022-09-28 10:51:27 +0530

    update

--- tf_dev/currencies.tf
+++ tf_dev/currencies.tf
@@ -5,11 +5,16 @@ provider "aws" {
 resource "aws_dynamodb_table" "ddbtable" {
   name             = "currencies-dev"
   hash_key         = "id"
+  range_key        = "RecordKey"
   billing_mode   = "PROVISIONED"
   read_capacity  = 5
   write_capacity = 5
   attribute {
     name = "id"
+    type = "S"
+  }
+    attribute {
+    name = "RecordKey"
     type = "S"
   }
 }
