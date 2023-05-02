fdfd468bec12c37bbb64476fe50d584fd32681e6
Leeroy Hendriks <leeroy@onramper.com>
2023-05-02 21:57:25 +0200

    Disable wallet prefill.

--- iframe/src/App.tsx
+++ iframe/src/App.tsx
@@ -177,6 +177,10 @@ function getArrayParam(paramName: string, defaultValue?: string[]) {
 }
 
 function getWalletsParam() {
+  if(apiKey === "pk_prod_trQ0nGBcmU_JY41N8Tl50Q00" || apiKey === "pk_test_oDsXkHokDdr06zZ0_sxJGw00"){
+    console.error(`Wallets param disabled.`);
+    return;
+  }
   return getParam("wallets", undefined)
     ?.split(",")
     .reduce((acc, wallet) => {
